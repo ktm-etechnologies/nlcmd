609c28ca3b3598dcaf3fbf5508e953fb38e5ab28
Dennis Huebner <Dennis.Huebner@KTM-technologies.com>
2019-09-27 11:35:55 +0200

    first

---
+++ markovspeech/src/main/java/com/ktm_technologies/markovspeech/JsonReader.java
@@ -0,0 +1,48 @@
+package com.ktm_technologies.markovspeech;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JsonReader {
+
+    public static MarkovChain read(InputStream in) {
+
+        byte[] buf = new byte[4096];
+        String content;
+
+        try {
+            int size;
+            StringBuilder sb = new StringBuilder();
+            while ((size = in.read(buf)) != -1) {
+                sb.append(new String(buf, 0, size, Config.CHARSET));
+            }
+            content = sb.toString();
+        } catch (IOException e) {
+            // TODO error handling
+            return null;
+        }
+
+        JSONObject model;
+        JSONArray edges;
+        int window;
+        try {
+            model = new JSONObject(content);
+            edges = model.optJSONArray(Config.JSON_EDGES);
+            window = model.optInt(Config.JSON_WINDOW, -1);
+            if (window == -1 || edges == null) {
+                // TODO error handling
+                return null;
+            }
+        } catch (JSONException e) {
+            // TODO error handling
+            return null;
+        }
+
+        MarkovChain mc = new MarkovChain(window);
+        return mc.load(edges) ? mc : null;
+    }
+}
