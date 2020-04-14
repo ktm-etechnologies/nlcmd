8b09043329b21e58230ea237a5f7fe6da2aa544c
r0b5t4 <56912+r0b5t4@users.noreply.github.com>
2020-04-14 23:37:32 +0200

    WIP CommandSet: Mixin subclass for custom scoring
    Fixes #21

--- nlcmd/src/main/java/com/ktm_technologies/nlcmd/CommandSet.java
+++ nlcmd/src/main/java/com/ktm_technologies/nlcmd/CommandSet.java
@@ -21,9 +21,36 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
+ * Custom scoring relative to phrase length
+ */
+class Mixin extends MarkovChainMixin {
+
+    @Override
+    int initQuery(List<String> phrase) {
+
+        int id = super.initQuery(phrase);
+        // TODO
+        return id;
+    }
+
+    @Override
+    void updateQuery(int id, Node node, Label label) {
+
+        // TODO
+    }
+
+    @Override
+    void finishQuery(int id) {
+
+        // TODO
+    }
+}
+
+/**
  * Represents a set of commands, with each command being represented by a
  * {@link MarkovChain}.
  */
+@SuppressWarnings("WeakerAccess")
 public class CommandSet extends HashMap<String, MarkovChain> {
 
     private int _order;
